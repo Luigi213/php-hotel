1e25f35e050aba9616e97bba596febd15a00aed5
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-30 14:04:45 +0100

    printed everything with parking control

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     $hotels = [
 
         [
@@ -46,9 +49,29 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <title>Document</title>
     </head>
     <body>
-        
+        <div>
+            <?php foreach($hotels as $hotel) { ?>
+                <div class="pb-4">
+                    <?php foreach($hotel as $key=>$item_hotel) { ?>
+                        <p>
+                            <?php if($key == 'parking') {
+                            if($item_hotel == true){
+                                echo $key.'=> Si';
+                            }            
+                            else{
+                                echo $key.'=> No';
+                            } ?>                
+                        <?php } else { 
+                            echo $key." => ".$item_hotel;
+                        } ?>
+                        </p>                        
+                    <?php } ?>    
+                </div>
+            <?php } ?>
+        </div>
     </body>
 </html>
